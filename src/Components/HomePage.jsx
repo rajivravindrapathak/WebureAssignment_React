307cf4199d9dceff0ad8638b015bd0a9da3f3336
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-02-05 09:47:23 +0530

    Add files via upload

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -17,12 +17,44 @@ import Countdown from "../Image/Countdown.svg"
 import dotimg from "../Image/dot1.svg"
 import dotimg1 from "../Image/dot.svg"
 import dotimg2 from "../Image/dot2.svg"
+import blueImg from "../Image/Rectangle3.svg"
+import penblue from "../Image/designthinking.svg"
+import textblue from "../Image/Contrary.svg"
+import Rectangle from "../Image/Rectangle.svg"
+import setting1 from "../Image/setting1.svg"
+import FrontEndDevelopment from "../Image/FrontEndDevelopment.svg"
+import setting13 from "../Image/setting13.svg"
+import BackEndDevelopment from "../Image/BackEndDevelopment.svg"
+import setting113 from "../Image/setting113.svg"
+import ApplicationDevelopment from "../Image/ApplicationDevelopment.svg"
+import setting12 from "../Image/setting12.svg"
+import CloudServices from "../Image/CloudServices.svg"
+import setting11 from "../Image/setting11.svg"
+import Analytics1 from "../Image/Analytics1.svg"
+import setting from "../Image/setting.svg"
+import QAAndTesting from "../Image/QAAndTesting.svg"
+import settingh from "../Image/settingh.svg"
+import BackOfficeSupport from "../Image/BackOfficeSupport.svg"
+import girl12 from "../Image/girl12.svg"
+import CosmeticForcestMarketing1 from "../Image/CosmeticForcestMarketing1.svg"
+import Rectangle123 from "../Image/Rectangle123.svg"
+import SalesAnalytic from "../Image/SalesAnalytic.svg"
+import Rectangle1041 from "../Image/Rectangle1041.svg"
+import FashionStoreMobileApps from "../Image/FashionStoreMobileApps.svg"
+import Rectangle10415 from "../Image/Rectangle10415.svg"
+import LaurasCloreBrandingIdentity from "../Image/LaurasCloreBrandingIdentity.svg"
+import Rectangle10416 from "../Image/Rectangle10416.svg"
+import SEOMarketing4 from "../Image/SEOMarketing4.svg"
+import PROJECTS1 from "../Image/PROJECTS1.svg"
+import SelectedWorksfromUs from "../Image/SelectedWorksfromUs.svg"
+import Rectangle10 from "../Image/Rectangle10.svg"
+import SeeMore from "../Image/SeeMore.svg"
 
 
 const HomePage = () => {
 
     return ( 
-        <div>
+        <div className="mainDiv">
             <h1>Homepage</h1>
 
             <div className="headDiv">
@@ -187,6 +219,84 @@ const HomePage = () => {
             <div className="text">
                 <h3>Our Services</h3>
                 <p>It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature</p>
+            </div>
+
+            <div className="mainblueDiv">
+                <div className="blueDiv">
+                    <img src={blueImg} />
+                    <img className="penblue" src={penblue} />
+                    <img className="textblue" src={textblue} />
+                </div>
+                <div className="bluesDiv">
+                    <img className="rectangle" src={Rectangle} alt="" />
+                    <img className="setting1" src={setting1} />
+                    <img className="FrontEndDevelopment" src={FrontEndDevelopment} alt="" />
+                </div>
+                <div>
+                    <img className="rectangle" src={Rectangle} alt="" />
+                    <img className="setting13" src={setting13} />
+                    <img className="BackEndDevelopment" src={BackEndDevelopment} alt="" />
+                </div>
+                <div>
+                    <img className="rectangle" src={Rectangle} alt="" />
+                    <img className="setting113" src={setting113} />
+                    <img className="ApplicationDevelopment" src={ApplicationDevelopment} alt="" />
+                </div>
+            </div>
+
+            <div className="mainblueDiv">
+                <div className="blueDiv">
+                    <img className="rectangle" src={Rectangle} alt="" />
+                    <img className="setting12" src={setting12} />
+                    <img className="CloudServices" src={CloudServices} />
+                </div>
+                <div className="bluesDiv">
+                    <img className="rectangle" src={Rectangle} alt="" />
+                    <img className="setting11" src={setting11} />
+                    <img className="Analytics1" src={Analytics1} alt="" />
+                </div>
+                <div>
+                    <img className="rectangle" src={Rectangle} alt="" />
+                    <img className="setting" src={setting} />
+                    <img className="QAAndTesting" src={QAAndTesting} alt="" />
+                </div>
+                <div>
+                    <img className="rectangle" src={Rectangle} alt="" />
+                    <img className="settingh" src={settingh} />
+                    <img className="BackOfficeSupport" src={BackOfficeSupport} alt="" />
+                </div>
+            </div>
+
+            <div className="girlDiv"> 
+                <div>
+                    <img className="girl12" src={girl12} alt="" /><br />
+                    <img className="CosmeticForcestMarketing1" src={CosmeticForcestMarketing1} />
+                </div>
+                <div>
+                    <img className="Rectangle123" src={Rectangle123} alt="" /><br />
+                    <img className="SalesAnalytic" src={SalesAnalytic} />
+                </div>
+                <div>
+                    <img className="Rectangle1041" src={Rectangle1041} alt="" /><br />
+                    <img className="FashionStoreMobileApps" src={FashionStoreMobileApps} />
+                </div>
+            </div>
+
+            <div className="girlDiv"> 
+                <div>
+                    <img className="Rectangle10415" src={Rectangle10415} alt="" /><br />
+                    <img className="LaurasCloreBrandingIdentity" src={LaurasCloreBrandingIdentity} />
+                </div>
+                <div>
+                    <img className="Rectangle10416" src={Rectangle10416} alt="" /><br />
+                    <img className="SEOMarketing4" src={SEOMarketing4} />
+                </div>
+                <div>
+                    <img className="PROJECTS1" src={PROJECTS1} alt="" /><br />
+                    <img className="SelectedWorksfromUs" src={SelectedWorksfromUs} />
+                    <img className="Rectangle10" src={Rectangle10} alt="" />
+                    <img className="SeeMore" src={SeeMore} alt="" />
+                </div>
             </div>
 
         </div>
